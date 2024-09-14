e85387449e1e859b7d1ff5519badcb2941534313
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2024-09-15 00:59:44 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Hello World</h1>
       <h1>ALok WOrking</h1>
       <h1>bikram WOrking</h1>
       <h1>First chnage on my branch</h1>
+      <h1>Hello</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
