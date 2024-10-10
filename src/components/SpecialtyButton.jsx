ca1ab493ba79b66bfc5b04f1d9ff098573563013
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2024-10-11 04:43:09 +0530

    commit

--- src/components/SpecialtyButton.jsx
+++ src/components/SpecialtyButton.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { Button } from '@mui/material';
+import React from "react";
+import { Button } from "@mui/material";
 
 function SpecialtyButton({ specialty }) {
   return (
-    <Button variant="contained" color="primary" style={{ margin: '0 8px 8px 0' }}>
+    <Button
+      variant="contained"
+      color="primary"
+      style={{ margin: "0 8px 8px 0" }}
+    >
       {specialty}
     </Button>
   );
